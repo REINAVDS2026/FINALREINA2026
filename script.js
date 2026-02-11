120d823c66a04ba9ab7d7c7b7642fc366c897bee
REINAVDS2026 <reinavds2025@gmail.com>
2026-02-10 19:22:24 -0600

    Refactor voting form submission handling

--- script.js
+++ script.js
@@ -18,12 +18,12 @@ document.getElementById("votingForm").addEventListener("submit", function(event)
   const voto = seleccion.value;
 
   const urls = [
-    "https://script.google.com/macros/s/AKfycbzcuA6W3UERKn0cIIsuTPZkZx2iIqL89VOsDfubUUfTxXgkacUphgX67NQH-2Wd-lI8xA/exec",
-    "https://script.google.com/macros/s/AKfycbzpNcPpgx807b8QMW11RWatvjWyTqCCJ00o4x42yyHR3qcxivZfvvNSmoLIw1BDjlh8/exec",
-    "https://script.google.com/macros/s/AKfycbxDUA7eV2TSdO-MAWp4S7YCstoS4LKylkuTUQ_a2z7Dvr3Mp2p3KQzoyz02RkWZmW7I/exec",
-    "https://script.google.com/macros/s/AKfycbyzvNn27Dgg5nf4ZpPni10iq3MoIshJUoT_qGFk4zKB8thDmlChBgWeW0ZtnniUt48p/exec",
-    "https://script.google.com/macros/s/AKfycbyrtI9nAkDALmDAxiomfIExhcNgok6BmWkrHiDWKHkQwYVdwOEc1MSNvAWky5mYlJKJ/exec",
-    "https://script.google.com/macros/s/AKfycbyz53OOKxgMndso-gDxH54iT5zTuq88GtENq_Qt3sFd8grnbRr1Lji3ifgz7yxrE2rKdA/exec"
+    "https://script.google.com/macros/s/AKfycbzFJIq4VBJBcgHQpetnpDZr2F-mODx2hTQgfeUShrCLLbeLHH7xFQe8Kn7-TSfAxMmL/exec",
+    //"https://script.google.com/macros/s/AKfycbzpNcPpgx807b8QMW11RWatvjWyTqCCJ00o4x42yyHR3qcxivZfvvNSmoLIw1BDjlh8/exec",
+    //"https://script.google.com/macros/s/AKfycbxDUA7eV2TSdO-MAWp4S7YCstoS4LKylkuTUQ_a2z7Dvr3Mp2p3KQzoyz02RkWZmW7I/exec",
+    //"https://script.google.com/macros/s/AKfycbyzvNn27Dgg5nf4ZpPni10iq3MoIshJUoT_qGFk4zKB8thDmlChBgWeW0ZtnniUt48p/exec",
+    //"https://script.google.com/macros/s/AKfycbyrtI9nAkDALmDAxiomfIExhcNgok6BmWkrHiDWKHkQwYVdwOEc1MSNvAWky5mYlJKJ/exec",
+    //"https://script.google.com/macros/s/AKfycbyz53OOKxgMndso-gDxH54iT5zTuq88GtENq_Qt3sFd8grnbRr1Lji3ifgz7yxrE2rKdA/exec"
   ];
 
   const url = urls[Math.floor(Math.random() * urls.length)];
@@ -38,3 +38,4 @@ document.getElementById("votingForm").addEventListener("submit", function(event)
   document.cookie = COOKIE_NAME + "=true; max-age=" + (60 * 60 * 24 * 30)+ "; path=/"; // 30 días
   window.location.href = "gracias.html";
 });
+
